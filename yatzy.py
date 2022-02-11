af3b716954a628c13ad8db6ee2b0933ebe2a4327
Rhys <Rhys.Jones@madetech.com>
2022-02-11 15:52:04 +0000

    Yatzy!

--- yatzy.py
+++ yatzy.py
@@ -1,145 +1,120 @@
 class Yatzy:
 
     def __init__(self, d1, d2, d3, d4, d5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = d5
+        self.dice = [d1, d2, d3, d4, d5]
 
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
-        rolls = [d1,d2,d3,d4,d5]
-        return sum(rolls)
+        return sum([d1, d2, d3, d4, d5])
 
     @staticmethod
     def yatzy(dice):
         return 50 if all(die == dice[0] for die in dice) else 0
-    
-    @staticmethod
-    def ones( d1,  d2,  d3,  d4,  d5):
-        rolls = [d1,d2,d3,d4,d5]
-        return rolls.count(1)
 
     @staticmethod
-    def twos( d1,  d2,  d3,  d4,  d5):
-        rolls = [d1,d2,d3,d4,d5]
-        return rolls.count(2)*2
-        
+    def ones(d1, d2, d3, d4, d5):
+        return Yatzy.calculate_singles(rolls=[d1, d2, d3, d4, d5], value=1)
+
     @staticmethod
-    def threes( d1,  d2,  d3,  d4,  d5):
-        rolls = [d1,d2,d3,d4,d5]
-        return rolls.count(3)*3
-    
+    def twos(d1, d2, d3, d4, d5):
+        return Yatzy.calculate_singles(rolls=[d1, d2, d3, d4, d5], value=2)
+
+    @staticmethod
+    def threes(d1, d2, d3, d4, d5):
+        return Yatzy.calculate_singles(rolls=[d1, d2, d3, d4, d5], value=3)
+
     def fours(self):
-        return self.dice.count(4)*4
-    
+        return self.calculate_singles(self.dice, value=4)
 
     def fives(self):
-        return self.dice.count(5)*5
-    
+        return self.calculate_singles(self.dice, value=5)
+
     def sixes(self):
-        return self.dice.count(6)*6
-    
-    @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
-        return 0
-    
-    @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
-    
-    @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
-        return 0
-    
+        return self.calculate_singles(self.dice, value=6)
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
-    
+    def calculate_singles(rolls, value):
+        return rolls.count(value) * value
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
+    def score_pair(d1, d2, d3, d4, d5):
+        rolls = [d1, d2, d3, d4, d5]
+
+        for value in range(6, 0, -1):
+            if rolls.count(value) == 2:
+                return value * 2
+        return 0
+
+    @staticmethod
+    def two_pair(d1, d2, d3, d4, d5):
+        rolls = [d1, d2, d3, d4, d5]
+
+        total = 0
+        for value in range(6, 0, -1):
+            if rolls.count(value) >= 2:
+                total += value * 2
+        return total if len(set(rolls)) <= 3 else 0
+
+    @staticmethod
+    def four_of_a_kind(_1, _2, d3, d4, d5):
+        tallies = [0] * 6
+        tallies[_1 - 1] += 1
+        tallies[_2 - 1] += 1
+        tallies[d3 - 1] += 1
+        tallies[d4 - 1] += 1
+        tallies[d5 - 1] += 1
+        for i in range(6):
+            if tallies[i] >= 4:
+                return (i + 1) * 4
+        return 0
+
+    @staticmethod
+    def three_of_a_kind(d1, d2, d3, d4, d5):
+        t = [0] * 6
+        t[d1 - 1] += 1
+        t[d2 - 1] += 1
+        t[d3 - 1] += 1
+        t[d4 - 1] += 1
+        t[d5 - 1] += 1
+        for i in range(6):
+            if t[i] >= 3:
+                return (i + 1) * 3
+        return 0
+
+    @staticmethod
+    def smallStraight(d1, d2, d3, d4, d5):
+        tallies = [0] * 6
+        tallies[d1 - 1] += 1
+        tallies[d2 - 1] += 1
+        tallies[d3 - 1] += 1
+        tallies[d4 - 1] += 1
+        tallies[d5 - 1] += 1
         if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
+                tallies[1] == 1 and
+                tallies[2] == 1 and
+                tallies[3] == 1 and
+                tallies[4] == 1):
             return 15
         return 0
-    
 
     @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
+    def largeStraight(d1, d2, d3, d4, d5):
+        tallies = [0] * 6
+        tallies[d1 - 1] += 1
+        tallies[d2 - 1] += 1
+        tallies[d3 - 1] += 1
+        tallies[d4 - 1] += 1
+        tallies[d5 - 1] += 1
         if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
+                tallies[2] == 1 and
+                tallies[3] == 1 and
+                tallies[4] == 1
+                and tallies[5] == 1):
             return 20
         return 0
-    
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
+    def fullHouse(d1, d2, d3, d4, d5):
         tallies = []
         _2 = False
         i = 0
@@ -147,24 +122,22 @@ class Yatzy:
         _3 = False
         _3_at = 0
 
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
+        tallies = [0] * 6
+        tallies[d1 - 1] += 1
+        tallies[d2 - 1] += 1
+        tallies[d3 - 1] += 1
+        tallies[d4 - 1] += 1
+        tallies[d5 - 1] += 1
 
         for i in range(6):
-            if (tallies[i] == 2): 
+            if (tallies[i] == 2):
                 _2 = True
-                _2_at = i+1
-            
+                _2_at = i + 1
 
         for i in range(6):
-            if (tallies[i] == 3): 
+            if (tallies[i] == 3):
                 _3 = True
-                _3_at = i+1
-            
+                _3_at = i + 1
 
         if (_2 and _3):
             return _2_at * 2 + _3_at * 3
