179930c5f111d3a472eb1c3c86912455affc62bc
Kate Riley <kate.riley@communities.gov.uk>
2022-02-11 15:07:13 +0000

    Refactored 2

--- yatzy.py
+++ yatzy.py
@@ -33,28 +33,14 @@ class Yatzy:
         return rolls.count(3)*3
     
     def fours(self):
-        sum = 0
-        for at in range(5):
-            if (self.dice[at] == 4): 
-                sum += 4
-        return sum
+        return self.dice.count(4)*4
     
 
     def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)): 
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
+        return self.dice.count(5)*5
     
-
     def sixes(self):
-        sum = 0
-        for at in range(len(self.dice)): 
-            if (self.dice[at] == 6):
-                sum = sum + 6
-        return sum
+        return self.dice.count(6)*6
     
     @staticmethod
     def score_pair( d1,  d2,  d3,  d4,  d5):
